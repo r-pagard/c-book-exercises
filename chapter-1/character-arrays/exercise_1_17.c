1826f3dbde9669c14b2b030d27002ab857de2c5f
richie <mohammadpagard.dev@gmail.com>
2025-02-05 23:59:57 +0100

    improve -Implemented a new exercise(Exercise 1-17) from Character Arrays topic of chapter-1.

---
+++ chapter-1/character-arrays/exercise_1_17.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+#define MAXLINE 100
+
+int getLine(char string[], int lim);
+
+
+int main() {
+    int length; // stores the length of the current input line
+    char line[MAXLINE]; // stores the current input line
+
+    while( (length = getLine(line, MAXLINE)) > 0 ) {
+        if(length - 1 >= 80) {  // check if the line length is greater than or equal to 80 characters
+            printf("Line (%d chars): %s", length - 1, line);    // print the line of its length is 80 characters or more
+        }
+    }
+
+    return 0;
+}
+
+int getLine(char string[], int lim) {
+    int character, length;
+
+    // read characters one by one and stores in the array
+    for(length = 0; length < lim-1 && (character = getchar()) != EOF && character != '\n'; ++length ) string[length] = character;
+    // if the loop exits due to newline, store the newline character in the array
+    if(character == '\n') {
+        string[length] = character;
+        ++length;
+    }
+
+    string[length] = '\0';  // null-terminate the string to make it a valid C string
+
+    return length;
+}
