dccfdcc1416112975efc8c488f7e4efe92157af2
richie <richiepagard@gmail.com>
2025-02-21 12:55:32 +0330

    improve -Improved Exercise 1-22, when print a group of a line, go through a line again and print the next group of a line and this is continue until encountered the newline of the input.

--- chapter-1/external-variables-scope/exercsise_1_22.c
+++ chapter-1/external-variables-scope/exercsise_1_22.c
@@ -28,22 +28,26 @@ void fold(void) {
 
     char character_group[MAXCHAR];  // buffer to store the input ine (up to MAXCHAR)
 
-    // read characters one by one and stores them in the array
-    for(position = 0; position < LIMIT && (character = getchar()) != EOF; ++position) character_group[position] = character;
+    // through on the line until the newline
+    while(character != '\n')
+    {
+        // read characters one by one and stores them in the array
+        for(position = 0; position < LIMIT && (character = getchar()) != EOF; ++position) character_group[position] = character;
 
-    // if the last character is a newline, store it and increment position
-    if(character == '\n') {
-        character_group[position] = character;
-        ++position;
+        // if the last character is a newline, store it and increment position
+        if(character == '\n') {
+            character_group[position] = character;
+            ++position;
+        }
+
+        // add null-terminated to make it a valid C string
+        character_group[position] = '\0';
+
+        // print the stored characters (line)
+        printf("%s\n", character_group);
+
+        // reset the buffer for the next line
+        character_group[0] = '\0';
+        position = 0;
     }
-
-    // add null-terminated to make it a valid C string
-    character_group[position] = '\0';
-
-    // print the stored characters (line)
-    printf("%s\n", character_group);
-
-    // reset the buffer for the next line
-    character_group[0] = '\0';
-    position = 0;
 }
