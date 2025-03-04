657769447af48b4d00213ac43fe04df8a6efab95
richie <richiepagard@gmail.com>
2025-03-05 01:49:52 +0330

    improve -Improved Exercise 1-23 in chapter-1 by complete the string and buffer. Wrote comments :/
    Not complete yet.

--- chapter-1/external-variables-scope/exercise_1_23.c
+++ chapter-1/external-variables-scope/exercise_1_23.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 
-#define MAXLINECHAR 50
+#define MAXTEMPLEN 50
+#define MAXLINECHAR 1000
 
 
 void comment_remover(void);
@@ -11,8 +12,9 @@ int main() {
 	comment_remover();
 
 
-    /*  Write a program to remove all comments from a C program. Don't forget to
-        handle quoted strings and character constants properly. C comments don't nest.
+    /* Write a program to remove all comments from a C program.
+       Don't forget to handle quoted strings and character constants properly.
+       C comments don't nest.
     */
 
     return 0;
@@ -22,16 +24,28 @@ int main() {
 void comment_remover(void) {
 	int character;
 	int position = 0;
-	char char_line[MAXLINECHAR];
+	char lines[MAXLINECHAR];
+	char temp[MAXTEMPLEN];
 
 	while( (character = getchar()) != EOF )
 	{
+		// Read characters into `lines` until newline and EOF
 		do {
-			char_line[position] = character;
+			// Store characters in the array and increment position
+			lines[position] = character;
 			position++;
 		}
 		while( character != '\n' && (character = getchar()) != EOF );
+
+		// If newline is encountered, store it in the array
+		if(character == '\n') {
+			lines[position] = character;
+			position++;
+		}
+		lines[position] = '\0';	// Null-terminate the string
 	}
 
-	printf("The Buffer: %s", char_line);
+	// print the buffer content
+	printf("\nThe Buffer: \n");
+	printf("\n%s\n", lines);
 }
